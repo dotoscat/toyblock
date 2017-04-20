b1662fe661a1c1a06eeb7a14e098ca106a9d130a
Oscar Triano dotoscat <user@example.com>
2017-04-20 23:36:29 +0200

    Use slots. Pool attributes are private

--- toyblock/pool.py
+++ toyblock/pool.py
@@ -1,24 +1,24 @@
 class Pool(object):
-    __slots__ = ('class_', 'avaliable', 'used')
+    __slots__ = ('__class_', '__avaliable', '__used')
     def __init__(self, class_, maxlen, *args, **kargs):
         from collections import deque
-        self.class_ = class_
-        self.avaliable = deque(
+        self.__class_ = class_
+        self.__avaliable = deque(
             [class_(*args, **kargs) for i in range(maxlen)],
             maxlen
             )
-        self.used = deque(maxlen=maxlen)
+        self.__used = deque(maxlen=maxlen)
 
     def get(self):
-        if not self.avaliable:
+        if not self.__avaliable:
             return None
-        element = self.avaliable.pop()
-        self.used.append(element)
+        element = self.__avaliable.pop()
+        self.__used.append(element)
         return element
 
     def free(self, element):
-        if not isinstance(element, self.class_) or element not in self.used:
+        if not isinstance(element, self.__class_) or element not in self.__used:
             return False
-        element = self.used.pop()
-        self.avaliable.append(element)
+        element = self.__used.pop()
+        self.__avaliable.append(element)
         return True
