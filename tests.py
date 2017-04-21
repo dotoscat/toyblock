4f8e3211886dd3d1a3896a8a7d91317c904ab719
Oscar Triano dotoscat <user@example.com>
2017-04-21 18:16:53 +0200

    Rename a test and add a free test for Pool

--- tests.py
+++ tests.py
@@ -8,7 +8,7 @@ class B:
     pass
 
 class PoolTest(unittest.TestCase):
-    def test_unique(self):
+    def test_get(self):
         unique = Pool(A, 1)
         instance = unique.get()
         self.assertEqual(isinstance(instance, A), True)
@@ -16,3 +16,8 @@ class PoolTest(unittest.TestCase):
     def test_empty(self):
         empty = Pool(A, 0)
         self.assertEqual(empty.get(), None)
+
+    def test_free(self):
+        pool = Pool(A, 3)
+        instance = pool.get()
+        self.assertTrue(pool.free(instance))
