da29d4a22cc78270d7e5d2f471bcebb8c28c7375
Oscar Triano dotoscat <user@example.com>
2017-04-23 20:13:59 +0200

    Add test for System

--- tests.py
+++ tests.py
@@ -45,4 +45,40 @@ class EntityTest(unittest.TestCase):
 
 class SystemTest(unittest.TestCase):
     def setUp(self):
-        self.system = System(A, B)
+
+        def update(a, b):
+            a.x += 1
+            b.x = a.x*2
+            #print(a.x, b.x)
+        
+        self.system = System(update, (A, B))
+        self.entities = [Entity() for i in range(100)]
+
+    def test1_add_entities(self):
+        for entity in self.entities:
+            self.assertTrue(entity.add_component(A, A()))
+            self.assertTrue(entity.add_component(B, B()))
+            self.assertTrue(self.system.add_entity(entity))
+        self.assertEqual(len(self.system._entities), 10)
+        for i in range(10):
+            self.system.run()
+        print(self.system._entities)
+
+    def test2_run_system(self):
+        print(self.system._entities)
+        for i in range(10):
+            self.system.run()
+
+def esto(uno, dos):
+    uno.x += 1
+    dos.x += 1
+
+class Ahuevo:
+    def __init__(self):
+        self.system = System(esto, (A, B))
+        self.entities = [Entity() for i in range(10)]
+        for i in self.entities:
+            i.add_component(A, A())
+            i.add_component(B, B())
+            self.system.add_entity(i)
+        
