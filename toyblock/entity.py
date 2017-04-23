9b503a5c0557b8e7860be86c7d69167c6a417348
Oscar Triano dotoscat <user@example.com>
2017-04-23 10:41:42 +0200

    Remove one underscore on Entity

--- toyblock/entity.py
+++ toyblock/entity.py
@@ -1,21 +1,21 @@
 class Entity(object):
-    __slots__ = ('__component')
+    __slots__ = ('_component')
     def __init__(self):
-        self.__component = {}
+        self._component = {}
 
     def add_component(self, class_, instance):
         if not isinstance(class_, type):
             return False
         if not isinstance(instance, class_):
             return False
-        if class_ in self.__component:
+        if class_ in self._component:
             return False
-        self.__component[class_] = instance
+        self._component[class_] = instance
         return True
 
     def get_component(self, class_):
-        return self.__component.get(class_)
+        return self._component.get(class_)
 
     def del_component(self, class_):
-        return self.__component.pop(class_, None)
+        return self._component.pop(class_, None)
     
