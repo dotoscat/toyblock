1fad85d8f144a9eb9ff8a3ca88890f8dd764a634
Oscar Triano dotoscat <user@example.com>
2017-05-03 18:45:40 +0200

    Remove System exception. Remove entity class check

--- toyblock/system.py
+++ toyblock/system.py
@@ -1,19 +1,3 @@
-class SystemError(Exception):
-    pass
-
-class SystemNotSatisfactoryEntityError(SystemError):
-    def __init__(self, system, class_, entity):
-        self.system = system
-        self.class_ = class_
-        self.entity = entity
-
-    def __str__(self):
-        return "{}: component {} not in entity {}".format(
-            self.system,
-            self.class_,
-            self.entity
-            )
-
 class System(object):
     __slots__ = ('_classes', '_callable_', '_entities', '_args', '_kwargs')
     def __init__(self, classes, callable_, *args, **kwargs):
@@ -25,17 +9,10 @@ class System(object):
         self._kwargs = kwargs
 
     def add_entity(self, entity):
-        classes = self._classes
-        for class_ in classes:
-            if class_ not in entity:
-                raise SystemNotSatisfactoryEntityError(self, class_, entity)
         self._entities.append(entity)
 
     def remove_entity(self, entity):
-        if entity not in self._entities:
-            return False
         self._entities.remove(entity)
-        return True
         
     def run(self):
         entities = self._entities
@@ -46,6 +23,9 @@ class System(object):
         for entity in entities:
             components = entity.get_components(classes)
             callable_(entity, *components, *args, **kwargs)
+
+    def __contains__(self, entity):
+        return entity in self._entities
 
     def __len__(self):
         return len(self._entities)
