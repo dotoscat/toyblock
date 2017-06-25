40b7385346b59ab1b61df310048c9bc8cfadc13a
Oscar Triano dotoscat <user@example.com>
2017-06-25 13:44:03 +0200

    Move Pool class to __init__.py

--- toyblock/__init__.py
+++ toyblock/__init__.py
@@ -16,8 +16,7 @@
 __all__ = ["Pool", "Entity", "System"]
 
 from collections import deque
-from . import pool, entity
-Pool = pool.Pool
+from . import entity
 Entity = entity.Entity
 
 class System(object):
@@ -105,3 +104,84 @@ def system(callable_):
     if not callable(callable_):
         raise TypeError("Use this as a DECORATOR")
     return System(callable_)
+
+class Pool(object):
+    """
+    A Pool is used for cache created objects and increase performance.
+    """
+    __slots__ = ("_avaliable", "_used", "_avaliable_pop",
+        "_used_append", "_used_remove", "_avaliable_append")
+    def __init__(self, maxlen, types, args_for_types=()):
+        """
+        Parameters:
+        
+        maxlen: Number of entities (Number).
+        types: Types for the entities (Sequence) with the classes.
+        args_for_types: Tuples of args for the instances of types
+        [(args, kwargs), ...]
+        
+        Example of use
+        
+        arguments = (
+            (
+                (1, 2),
+                None
+            ),
+            (
+                None,
+                {"a": 7}
+            ),
+            None
+        )
+        
+        pool = toyblock.Pool(10, (A, B, C), arguments)
+        
+        """
+        try:
+            from itertools import zip_longest
+        except:
+            from itertools import izip_longest as zip_longest
+        Entity = entity.Entity
+        self._avaliable = deque(maxlen=maxlen)
+        avaliable_append = self._avaliable.append
+        EMPTY_TUPLE = ()
+        EMPTY_DICT = {}
+        EMPTY_ARGS = (None, None)
+        for i in range(maxlen):
+            entity = Entity()
+            entity_add_component = entity.add_component
+            for pair in zip_longest(types, args_for_types):
+                type_, type_args = pair
+                args, kwargs = EMPTY_ARGS if type_args is None else type_args
+                args = EMPTY_TUPLE if args is None else args
+                kwargs = EMPTY_DICT if kwargs is None else kwargs
+                instance = type_(*args, **kwargs)
+                entity_add_component(instance)
+            avaliable_append(entity)
+        self._used = deque(maxlen=maxlen)
+        
+        #Remap methods to be used directly
+        self._avaliable_pop = self._avaliable.pop
+        self._used_append = self._used.append
+        self._used_remove = self._used.remove
+        self._avaliable_append = self._avaliable.append
+
+    def get(self):
+        """Return a free instance if avaliable, None otherwise.
+        """
+        if not self._avaliable:
+            return None
+        element = self._avaliable_pop()
+        self._used_append(element)
+        return element
+
+    def free(self, element):
+        """Mark the instance to be avaliable and return True.
+
+        Return False if it do not belong to the pool or is not used yet.
+        """
+        if element not in self._used:
+            return False
+        self._used.remove(element)
+        self._avaliable.append(element)
+        return True
